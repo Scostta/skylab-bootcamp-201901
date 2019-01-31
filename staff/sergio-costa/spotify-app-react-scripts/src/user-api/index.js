7c5a2251b78538bd4b376cbe957e2582870fc291
Scostta <sergio.cs04@gmail.com>
2019-01-31 20:53:02 +0100

    update

--- staff/sergio-costa/spotify-app-react-scripts/src/user-api/index.js
+++ staff/sergio-costa/spotify-app-react-scripts/src/user-api/index.js
@@ -1,26 +1,112 @@
 'use strict'
 
 const userApi = {
+
     register(username, password) {
-        if(typeof username !== 'string')throw TypeError (`${username} is not a string`)
-        if(username.trim().length)throw Error('username is empty')
+        if (typeof username !== 'string') throw TypeError(`${username} is not a string`)
+        if (!username.trim().length) throw Error('username is empty')
 
-        if(typeof password !== 'string')throw TypeError(`${password} is not a string`)
-        if(password.trim().length)throw Error('password is empty')
+        if (typeof password !== 'string') throw TypeError(`${password} is not a string`)
+        if (!password.trim().length) throw Error('password is empty')
 
-        return fetch('https://skylabcoders.herokuapp.com/api/user', {
-            method: 'POST',
-            header: {
+        return fetch('https://skylabcoders.herokuapp.com/api/user', { 
+            method: 'POST', 
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({ username, password })
+        })
+            .then(response => response.json())
+            .then(response => {
+                const { status } = response
+                
+                if (status === 'OK') return response.data.id
+                else throw Error(response.error)
+            })
+    },
+
+    auth(username, password){
+        if (typeof username !== 'string') throw TypeError(`${username} is not a string`)
+        if (!username.trim().length) throw Error('username is empty')
+
+        if (typeof password !== 'string') throw TypeError(`${password} is not a string`)
+        if (!password.trim().length) throw Error('password is empty')
+
+        return fetch('https://skylabcoders.herokuapp.com/api/auth', { 
+            method: 'POST', 
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({ username, password })
+        })
+            .then(response => response.json())
+            .then(response => {
+                const { status, data } = response
+
+                if(status === 'OK'){
+                    return data
+                }else{
+                    throw Error(response.error)
+                }
+            })
+    },
+
+    retrieve(id, token){
+        return fetch(`https://skylabcoders.herokuapp.com/api/user/${id}`, {
+            method:'GET',
+            headers: {
+                authorization: `Bearer ${token}`
+            }
+        })
+            .then(response => response.json())
+            .then(response => {
+                const {status, data} = response
+
+                if(status=='OK'){
+                    return data
+                }else{
+                    throw Error(response.error)
+                }
+            }) 
+    },
+
+    update(id, token, moreInfo){
+        return fetch(`https://skylabcoders.herokuapp.com/api/user/${id}`, {
+            method:'PUT',
+            headers: {
+                authorization: `Bearer ${token}`,
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(moreInfo)
+        })
+            .then(response => response.json())
+            .then(response => {
+                const{ data, status } = response
+
+                if(status === 'OK') return data
+                else{
+                    throw Error(response.error)
+                }
+            })
+    },
+
+    remove(id, token, username, password){
+        return fetch(`https://skylabcoders.herokuapp.com/api/user/${id}`, {
+            method:'DEL',
+            headers: {
+                authorization: `Bearer ${token}`,
                 'content-type': 'application/json'
             },
             body: JSON.stringify({username, password})
         })
             .then(response => response.json())
             .then(response => {
-                const {status} = response
+                const {status, data} = response
 
-                if(status === 'OK') return response.data.id
-                else throw Error(response.error)
+                if(status === 'OK') return data
+                else{
+                    throw Error(response.error)
+                }
             })
     }
 }
--- staff/sergio-costa/spotify-app-react-scripts/src/user-api/index.spec.js
+++ staff/sergio-costa/spotify-app-react-scripts/src/user-api/index.spec.js
@@ -1,27 +1,135 @@
 'use strict'
 
-import userAPi from '.'
+import userApi from '.'
 
-describe('user API', () => {
-    const username = `manuelbarzi-${Math.random}`
+describe('user api', () => {
+    const username = `sergio-${Math.random()}`
     const password = '123'
 
     describe('register', () => {
-        it('should succeed on correct data', () => {
-            userAPi.register(username, password)
+
+        it('should succeed on correct data', () =>
+            userApi.register(username, password)
                 .then(id => expect(id).toBeDefined())
                 .catch(error => expect(error).toBeUndefined())
-        })
+        )
 
-        it('should fail on already existing user', () => {
-            userAPi.register(username. password)
+        it('should fail on already existing user', () =>
+            userApi.register(username, password)
                 .then(() => {
                     throw Error('should not have passed by here')
                 })
                 .catch(error => {
                     expect(error).toBeDefined()
-                    expect(error.message).toBe(`user with username \"${username}"\ already exists`)
+                    expect(error.message).toBe(`user with username \"${username}\" already exists`)
                 })
-        })
+        )
+
     })
+
+    describe('auth', () => {
+
+        it('should return id and token', () =>
+            userApi.auth(username, password)
+                .then(data => {
+                    expect(data.id).toBeDefined()
+                    expect(data.token).toBeDefined()
+                })
+                .catch(error => expect(error).toBeUndefined())
+        )
+
+        it('should not return a token', () =>
+            userApi.auth('hola', password)
+                .then(() => {
+                    throw Error('should no have passed by here')
+                })
+                .catch(error => {
+                    expect(error).toBeDefined()
+                    expect(error.message).toBe(`user with username \"${'hola'}\" does not exist`)
+                })
+        )
+    })
+
+    describe('retrieve', () => {
+
+        it('should return username and id', () =>
+
+            userApi.auth(username, password)
+                .then(data => {
+                    return userApi.retrieve(data.id, data.token)
+                        .then(user => {
+                            expect(user.id).toBeDefined()
+                            expect(user.username).toBeDefined()
+                            expect(user.username).toBe(username)
+                        })
+                        .catch(error => {
+                            expect(error).toBeUndefined()
+                        })
+                })
+        )
+
+        it('should fail if the id is incorrect', () =>
+
+            userApi.auth(username, password)
+                .then(data => {
+                    return userApi.retrieve('sadasdasd', data.token)
+                        .then(() => {
+                            throw Error('should not have passed by here')
+                        })
+                        .catch(error => {
+                            expect(error).toBeDefined()
+                            expect(error.message).toBe(`token id \"${data.id}\" does not match user \"sadasdasd\"`)
+                        })
+                })
+        )
+
+        it('should fail if the token is null or incorrect', () =>
+
+            userApi.auth(username, password)
+                .then(data => {
+                    return userApi.retrieve(data.id, )
+                        .then(() => {
+                            throw Error('should not have passed by here')
+                        })
+                        .catch(error => {
+                            expect(error).toBeDefined()
+                            expect(error.message).toBe('invalid token')
+                        })
+                })
+        )
+
+    })
+
+    describe('update', () => {
+
+        it('should add new info', () => 
+            userApi.auth(username, password)
+                .then(data => {
+                    return userApi.update(data.id, data.token, 'surname: costa')
+                        .then(user => {
+                            expect(user.surname).toBe('costa')
+                        })
+                })
+        
+        )
+
+        it('should change the username', () => 
+        userApi.auth(username, password)
+                .then(data=> {
+                    return userApi.update(data.id, data.token, 'username: hello')
+                        .then(user => {
+                            expect(user.username).toBe('hello')
+                        })
+                })
+        )
+    })
+
+    describe('remove', () => 
+    userApi.auth(username, password)
+        .then(data => {
+            return userApi.remove(data.id, data.token, username, password)
+                .then()
+        })
+    
+    )
 })
